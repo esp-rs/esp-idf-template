6115cd9c97e2a48ba8cd6d1a9beda481666604f5
imarkov <imarkov@vmware.com>
2021-10-25 11:08:11 -0400

    Fix a typo

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 use esp_idf_sys;
 
 fn main() {
-    // Temporary. Will disapper once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
+    // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
     // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
     esp_idf_sys::link_patches();
 
