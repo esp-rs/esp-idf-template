9eb8830a1a27a336a4e7c93df06b3b892c8d1c25
imarkov <imarkov@vmware.com>
2021-10-31 22:41:57 +0200

    Fix formatting

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@
 #![no_main]
 
 {% endunless %}#[allow(unused_imports)]
-use esp_idf_sys; // If using the `binstart` feature of `esp-idf-sys`, always keep the module imported
-{% unless std %}use esp_idf_svc;
+use esp_idf_sys; // If using the `binstart` feature of `esp-idf-sys`, always keep the module imported{% unless std %}
+use esp_idf_svc;
 
 use log::*;{% endunless %}
 
@@ -12,7 +12,6 @@ use log::*;{% endunless %}
     // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
     // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
     esp_idf_sys::link_patches();
-
 {% if std %}
     println!("Hello, world!");{% else %}
     // Bind the log crate to the ESP Logging facilities
