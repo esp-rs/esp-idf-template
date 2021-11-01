c4b7c0ae82a0a146024aecba68c24bae10307a2e
imarkov <imarkov@vmware.com>
2021-11-01 08:40:28 +0200

    Fix formatting

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 {% unless std %}#![no_std]
 #![no_main]
 
-{% endunless %}#[allow(unused_imports)]
+use esp_idf_svc;{% endunless %}#[allow(unused_imports)]
 use esp_idf_sys; // If using the `binstart` feature of `esp-idf-sys`, always keep the module imported{% unless std %}
-use esp_idf_svc;
 
-use log::*;{% endunless %}
+{% unless std %}use log::*;
 
-{% unless std %}#[no_mangle]
+#[no_mangle]
 {% endunless %}fn main() {
     // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
     // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
