d8460770158bb2857eac34caaede9d2b2d360f29
imarkov <imarkov@vmware.com>
2021-11-01 13:36:09 +0200

    Use latest esp-idf-sys to fix no_std builds on RiscV; take cfg parameters

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 // Necessary because of this issue: https://github.com/rust-lang/cargo/issues/9641
 fn main() -> anyhow::Result<()> {
+    embuild::build::CfgArgs::output_propagated("ESP_IDF")?;
     embuild::build::LinkArgs::output_propagated("ESP_IDF")
 }
