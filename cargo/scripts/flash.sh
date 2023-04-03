f829a8fee29376c63cbf01199664f868a8cb7b6f
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-04-03 16:53:08 +0200

    feat: 🔥 Remove gitpod support

--- cargo/scripts/flash.sh
+++ cargo/scripts/flash.sh
@@ -4,24 +4,18 @@ set -e
 
 BUILD_MODE=""
 case "$1" in
-    ""|"release")
-        bash scripts/build.sh
-        BUILD_MODE="release"
-        ;;
-    "debug")
-        bash scripts/build.sh debug
-        BUILD_MODE="debug"
-        ;;
-    *)
-        echo "Wrong argument. Only \"debug\"/\"release\" arguments are supported"
-        exit 1;;
+"" | "release")
+    bash scripts/build.sh
+    BUILD_MODE="release"
+    ;;
+"debug")
+    bash scripts/build.sh debug
+    BUILD_MODE="debug"
+    ;;
+*)
+    echo "Wrong argument. Only \"debug\"/\"release\" arguments are supported"
+    exit 1
+    ;;
 esac
 
-export ESP_ARCH=
-{%- if mcu == "esp32c3" -%}
-riscv32imc-esp-espidf
-{%- else -%}
-xtensa-{{ mcu }}-espidf
-{%- endif %}
-
-web-flash --chip {{ mcu }} target/${ESP_ARCH}/${BUILD_MODE}/{{ project-name }}
+web-flash --chip {{ mcu }} target/{{ rust_target }}/${BUILD_MODE}/{{ project-name }}
