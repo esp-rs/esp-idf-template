5dfc74c6163f5330cca907e238b09d9046082e26
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-05-12 15:02:29 +0200

    fix: 🐛 Only use log when using hals

--- cargo/src/main.rs
+++ cargo/src/main.rs
@@ -14,9 +14,9 @@ fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_sys::link_patches();
-{%- if std %}
+{%- if std and hal == "No" %}
     println!("Hello, world!");
-{%- elsif hal == "Yes (default features)" or hal == "Yes (all features)" %}
+{%- elsif std == false hal != "No"  %;}
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
