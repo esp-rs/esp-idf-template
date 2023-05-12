b61b50efbdfb9f647f703a45a28ebd4f76c359bb
ivmarkov <ivan.markov@gmail.com>
2023-05-12 18:09:22 +0000

    Port the removal of anyhow from cargo to cmake

--- cmake/components/rust-{{project-name}}/build.rs
+++ cmake/components/rust-{{project-name}}/build.rs
@@ -1,4 +1,5 @@
 // Necessary because of this issue: https://github.com/rust-lang/cargo/issues/9641
-fn main() -> Box<dyn std::error::Error> {
-    embuild::build::CfgArgs::output_propagated("ESP_IDF")
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    embuild::build::CfgArgs::output_propagated("ESP_IDF")?;
+    Ok(())
 }
