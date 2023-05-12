b16aa6dd400a66273d6be80fbf00a7b5763525f4
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-05-12 16:45:21 +0200

    fix: 🐛 Fix cmake conditions

--- cmake/components/rust-{{project-name}}/src/lib.rs
+++ cmake/components/rust-{{project-name}}/src/lib.rs
@@ -1,7 +1,8 @@
-{% unless std %}#![no_std]
-
-{% endunless %}use esp_idf_sys as _; // If using the `libstart` feature of `esp-idf-sys`, always keep this module imported
-{% if not std and (hal == "Yes (default features)" or hal == "Yes (all features)") %}
+{% unless std -%}
+#![no_std]
+{% endunless -%}
+use esp_idf_sys as _; // If using the `libstart` feature of `esp-idf-sys`, always keep this module imported
+{%- if std and hal != "No" %}
 use log::*;
 {% endif %}
 
@@ -10,12 +11,14 @@ extern "C" fn rust_main() -> i32 {
     // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
     // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
     esp_idf_sys::link_patches();
-{% if std %}
-    println!("Hello world from Rust!");{% elsif hal == "Yes (default features)" or hal == "Yes (all features)" %}
+{%- if std and hal == "No" %}
+    println!("Hello world from Rust!");
+{%- elsif std and hal != "No"  %}
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    info!("Hello world from Rust!");{% endif %}
+    info!("Hello world from Rust!");
+{%- endif %}
 
     42
 }
