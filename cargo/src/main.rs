de9a583f994a1866019ab568c2f21a02ae9db823
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-05-12 16:04:32 +0200

    fix: 🐛 Fix condition

--- cargo/src/main.rs
+++ cargo/src/main.rs
@@ -3,7 +3,7 @@
 #![no_main]
 {% endunless -%}
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
-{%- if std == false and hal != "No" %}
+{%- if std and hal != "No" %}
 use log::*;
 {% endif %}
 
