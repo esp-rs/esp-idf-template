02f5f49f20d6232b3666a2c290a3fa890f415e0a
ivmarkov <ivan.markov@gmail.com>
2023-10-18 04:39:43 +0000

    Get rid of blank spaces

--- cargo/src/main.rs
+++ cargo/src/main.rs
@@ -5,7 +5,7 @@
 
 {%- unless std %}
 #[no_mangle]
-{%- endunless %}
+{%- endunless -%}
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
