7fb8feb121c77c17e2bc95204b59f7fe377e7316
ivmarkov <ivan.markov@gmail.com>
2023-10-18 10:37:56 +0300

    Fix formatting

--- cargo/src/main.rs
+++ cargo/src/main.rs
@@ -3,9 +3,9 @@
 #![no_main]
 
 {% endunless -%}
-{%- unless std %}
+{% unless std -%}
 #[no_mangle]
-{%- endunless -%}
+{% endunless -%}
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
